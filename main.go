8746f42b105a180be97b736cca85f6c45e81986b
SerumYouNeed <greg.miler@gmail.com>
2026-06-12 23:55:26 +0200

    mmm

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 func main () {
 	fmt.Println("Hello World!")
 
+	var x int = 5
+	fmt.Printf("The value of x is %d\n", x)
+
 	for i := 0; i < 10; i++ {
 		fmt.Printf("The value of i is %d\n", i)
 	}	
