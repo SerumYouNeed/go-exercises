964754be44ece9fbd5197d6390a77d2f8c4bb9d5
SerumYouNeed <greg.miler@gmail.com>
2026-06-24 23:38:02 +0200

    24.06

--- main.go
+++ main.go
@@ -7,14 +7,6 @@ import (
 
 func main () {
 
-	for _, v := range []int{1, 2, 3, 4, 5, 6} {
-
-		fmt.Printf("%d ", v)
-		if v == 5 {
-			continue
-		}
-	}
-
 	z := 10000
 	if z > 0 {
 		fmt.Println("z is bigger than 0")
