590dcc61b9d30de8f30ac79593bc62ae678b5374
SerumYouNeed <greg.miler@gmail.com>
2026-09-25 23:45:40 +0200

    packages

--- 02_variables_and_types/main.go
+++ 02_variables_and_types/main.go
@@ -1,1 +1,21 @@
-bbb
+package main
+
+import "fmt"
+
+func main(){
+	var channelName string
+	channelName = "Learn Golang"
+	var year int = 2026
+
+	fmt.Println("Chanel:", channelName)
+	fmt.Println("Year:", year)
+
+	var rating float64 = 3.4
+	fmt.Println("Rating:", rating)
+
+	var isActive = true // type is isferred from the value
+	fmt.Println("Is Active:", isActive)
+
+	guestCount := 10 // short declaration, type is inferred
+	fmt.Println("Guest Count:", guestCount)
+}
